fea19f2f1acb00b22c2969683b5384df978fa221
winstxnhdw <winstxnhdw@gmail.com>
2023-10-02 05:38:31 +0800

    feat: add `/delete_chat` endpoint

---
+++ server/api/v1/delete_chat.py
@@ -0,0 +1,24 @@
+
+from typing import Annotated
+
+from fastapi import Depends
+from redis import Redis
+
+from server.api.v1 import v1
+from server.dependencies import get_redis_client
+
+
+@v1.get('/{chat_id}/delete_chat')
+async def delete_chat(
+    chat_id: str,
+    redis: Annotated[Redis, Depends(get_redis_client)],
+):
+    """
+    Summary
+    -------
+    the `/delete_chat` route provides an endpoint for deleting a chat from the Redis database
+    """
+    for key in redis.scan_iter():
+        tag = await redis.hget(key, 'tag')  # type: ignore
+        if tag == chat_id.encode('utf-8'):
+            redis.delete(key)  # type: ignore
