a82c786f4323094b8f7d52147f42c07fe6d649ce
winstxnhdw <winstxnhdw@gmail.com>
2023-10-02 05:39:06 +0800

    fix: `delete_file` should contain redis index prefix

--- server/api/v1/delete_file.py
+++ server/api/v1/delete_file.py
@@ -5,11 +5,12 @@ from fastapi import Depends
 from redis import Redis
 
 from server.api.v1 import v1
+from server.config import Config
 from server.dependencies import get_redis_client
 
 
 @v1.get('/delete_file/{file_id}')
-def delete_file(
+async def delete_file(
     file_id: str,
     redis: Annotated[Redis, Depends(get_redis_client)],
 ):
@@ -18,4 +19,4 @@ def delete_file(
     -------
     the `/delete_file` route provides an endpoint for deleting a file from the Redis database
     """
-    redis.delete(file_id)
+    return await redis.delete(f'{Config.document_index_prefix}:{file_id}')
