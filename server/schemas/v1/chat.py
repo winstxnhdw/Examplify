ee478e27841268a3b542ce1b28cda7317ece9050
winstxnhdw <winstxnhdw@gmail.com>
2023-10-03 21:49:08 +0800

    fix: `uuid` should not be splitted in pydantic examples

--- server/schemas/v1/chat.py
+++ server/schemas/v1/chat.py
@@ -11,4 +11,4 @@ class Chat(BaseModel):
     ----------
     chat_id (str) : the chat id
     """
-    chat_id: str = Field(examples=['a8ae18389ed34ca391cdcc0160448df8'])
+    chat_id: str = Field(examples=['6a67cb2e-a618-46b1-b617-73a0f3805122'])
--- server/schemas/v1/uploaded.py
+++ server/schemas/v1/uploaded.py
@@ -12,7 +12,7 @@ class DocumentSchema(BaseModel):
     id (str) : the document id
     name (str) : the document name
     """
-    id: str = Field(examples=['a8ae18389ed34ca391cdcc0160448df8'])
+    id: str = Field(examples=['6a67cb2e-a618-46b1-b617-73a0f3805122'])
     name: str = Field(examples=["Stephen S. Carey - A Beginner's to Scientific Method, 4th Edition"])
 
 
