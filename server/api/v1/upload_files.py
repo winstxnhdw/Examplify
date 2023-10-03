261f78312c3b5ff3cd1801e84ba554b5079d6ee5
winstxnhdw <winstxnhdw@gmail.com>
2023-10-03 20:29:41 +0800

    perf: use `gather` to `hset` concurrently

--- server/api/v1/upload_files.py
+++ server/api/v1/upload_files.py
@@ -10,8 +10,9 @@ from server.api.v1 import v1
 from server.config import Config
 from server.dependencies import get_redis_client
 from server.features import LLM, Embedding, chunk_document
-from server.features.chunking.sentence_splitter import SentenceSplitter
+from server.features.chunking import SentenceSplitter
 from server.features.extraction import extract_texts_from_requests
+from server.schemas.v1 import DocumentSchema, Uploaded
 
 
 @v1.post('/{chat_id}/upload_files')
@@ -19,27 +20,36 @@ async def upload_files(
     chat_id: str,
     requests: list[UploadFile],
     redis: Annotated[Redis, Depends(get_redis_client)]
-):
+) -> Uploaded:
     """
     Summary
     -------
     the `/upload_files` route provides an endpoint for uploading a file to the server
     """
     embedder = Embedding()
+    response: list[DocumentSchema | None] = [None] * len(requests)
     text_splitter = SentenceSplitter(LLM.tokeniser, chunk_size=128, chunk_overlap=0)
 
     async with redis.pipeline() as pipeline:
-        for document in extract_texts_from_requests(requests):
+        for i, document in enumerate(extract_texts_from_requests(requests)):
             if not document:
                 raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, 'No file name!')
 
-            for chunk in chunk_document(document, text_splitter):
+            response[i] = DocumentSchema(
+                id=document.id,
+                name=document.semantic_identifier
+            )
+
+            hashset_coroutines = [
                 pipeline.hset(f'{Config.document_index_prefix}:{chunk.source_id}-{chunk.id}', mapping={
                     'vector': embedder.encode_normalise(chunk.content).tobytes(),
                     'content': chunk.content,
                     'tag': chat_id
-                })
+                }) for chunk in chunk_document(document, text_splitter)
+            ]
+
+            await gather(*hashset_coroutines) # type: ignore
 
         await pipeline.execute()
 
-    # return [document.id for document in documents]
+    return Uploaded(documents=response)
