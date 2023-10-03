6c12b6de77b485fa157fa3950b81b30ee6473094
winstxnhdw <winstxnhdw@gmail.com>
2023-10-03 21:20:26 +0800

    fix: preserve system messages on long messages

--- server/schemas/v1/query.py
+++ server/schemas/v1/query.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from pydantic import Field
 
 from server.features.llm.types import Message
@@ -12,9 +14,9 @@ class Query(Search):
 
     Attributes
     ----------
-    messages (list[Message]) : the list of previous messages
+    messages (deque[Message]) : the list of previous messages
     """
-    messages: list[Message] = Field(examples=[[
+    messages: deque[Message] = Field(examples=[[
         {
             'role': 'user',
             'content': 'You are a helpful AI assistant. You are given the following chat history. Answer the question based on the context provided as truthfully as you are able to. If you do not know the answer, you may respond with "I do not know". What is the Baloney Detection Kit?'
@@ -23,4 +25,4 @@ class Query(Search):
             'role': 'assistant',
             'content': 'The Baloney Detection Kit is a a set of cognitive tools and techniques that fortify the mind against penetration by falsehoods. It was created by Carl Sagan.'
         },
-    ]])
+    ]], default=[])
