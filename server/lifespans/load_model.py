2e44b67230b6583aa2c37bc8fe3913231ee4938c
winstxnhdw <winstxnhdw@gmail.com>
2023-10-04 17:08:02 +0800

    refactor: remove redundant helper

--- server/lifespans/load_model.py
+++ server/lifespans/load_model.py
@@ -3,15 +3,6 @@ from asyncio import get_running_loop
 from server.features import LLM
 
 
-def load_language_model():
-    """
-    Summary
-    -------
-    download and load the language model
-    """
-    LLM.load()
-
-
 async def load_model():
     """
     Summary
@@ -20,5 +11,5 @@ async def load_model():
     """
     await get_running_loop().run_in_executor(
         None,
-        load_language_model
+        LLM.load
     )
