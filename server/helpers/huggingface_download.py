901011e2f0c85b64a72bf6277c8396dcfacd3811
winstxnhdw <winstxnhdw@gmail.com>
2023-10-06 02:35:37 +0800

    fix: disable progress bar for embeddings

--- server/helpers/huggingface_download.py
+++ server/helpers/huggingface_download.py
@@ -1,6 +1,19 @@
 from http.client import HTTPConnection
+from typing import Any
 
 from huggingface_hub import snapshot_download
+from tqdm.asyncio import tqdm_asyncio
+
+
+class DisableTqdm(tqdm_asyncio):  # type: ignore
+    """
+    Summary
+    -------
+    a class that disables the tqdm progress bar
+    """
+    def __init__(self, *args: Any, **kwargs: Any):
+        kwargs['disable'] = True
+        super().__init__(*args, **kwargs)
 
 
 def has_internet_access() -> bool:
@@ -22,10 +35,15 @@ def has_internet_access() -> bool:
         connection.close()
 
 
-def huggingface_download(repository: str) -> str:
+def huggingface_download(repository: str, enable_progress_bar: bool = True) -> str:
     """
     Summary
     -------
     download the huggingface model
     """
-    return snapshot_download(repository, resume_download=True, local_files_only=not has_internet_access())
+    return snapshot_download(
+        repository,
+        resume_download=True,
+        local_files_only=not has_internet_access(),
+        tqdm_class=None if enable_progress_bar else DisableTqdm  # type: ignore
+    )
