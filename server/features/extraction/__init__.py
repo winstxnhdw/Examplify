2c8a17853a119e519f78fc5d0ceed1ccc66efcd7
khayliang <khayliang@gmail.com>
2023-11-21 01:33:14 +0800

    feat: add text from image extractor

--- server/features/extraction/__init__.py
+++ server/features/extraction/__init__.py
@@ -1,3 +1,4 @@
 from server.features.extraction.extract_text import (
     extract_texts_from_requests as extract_texts_from_requests,
+    extract_texts_from_image_requests as extract_texts_from_image_requests
 )
--- server/features/extraction/extract_text.py
+++ server/features/extraction/extract_text.py
@@ -4,6 +4,8 @@ from uuid import uuid4
 from fastapi import UploadFile
 from fitz import Document as FitzDocument
 
+from tesserocr import PyTessBaseAPI
+
 from server.features.extraction.models import Document
 from server.features.extraction.models.document import Section
 
@@ -36,6 +38,30 @@ def extract_text(file_name: str, file_type: str, file: bytes) -> Document:
         semantic_identifier=file_name
     )
 
+def extract_texts_from_image(file_name: str, file: bytes) -> Document:
+    """
+    Summary
+    -------
+    extract the text from an image
+
+    Parameters
+    ----------
+    file_name (str): the name of the file
+    file (bytes): the file
+
+    Returns
+    -------
+    document (Document): the parsed document
+    """
+    with PyTessBaseAPI() as ocr:
+        with Image.open(file) as img:
+            sections = [ocr.image_to_text(img)]
+
+    return Document(
+        id=str(uuid4()),
+        sections=sections,
+        semantic_identifier=file_name
+    )
 
 def extract_texts_from_requests(requests: list[UploadFile]) -> Generator[Document | None, None, None]:
     """
@@ -54,6 +80,27 @@ def extract_texts_from_requests(requests: list[UploadFile]) -> Generator[Documen
     for request in requests:
         yield (
             extract_text(*request.filename.rsplit('.', 1), file=request.file.read())
+            if request.filename
+            else None
+        )
+
+def extract_texts_from_image_requests(requests: list[UploadFile]) -> Generator[Document | None, None, None]:
+    """
+    Summary
+    -------
+    extract the text from a list of requests containing images
+
+    Parameters
+    ----------
+    requests (list[UploadFile]): the requests to extract the text from
+
+    Yields
+    ------
+    documents (Document): the parsed document
+    """
+    for request in requests:
+        yield (
+            extract_texts_from_image(request.filename.rsplit('.', 1)[0], file=request.file.read())
             if request.filename
             else None
         )
