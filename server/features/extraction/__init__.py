0f5c033fdf27a217e53423904c62b8ab0439fb21
khayliang <khayliang@gmail.com>
2023-11-22 01:23:23 +0800

    refactor: rename pdf text extractor file

--- server/features/extraction/__init__.py
+++ server/features/extraction/__init__.py
@@ -1,4 +1,4 @@
-from server.features.extraction.extract_text import (
+from server.features.extraction.extract_from_pdf import (
     extract_texts_from_requests as extract_texts_from_requests,
 )
 
