3933be369e9a6b4d688d710b231167b3d9f40863
winstxnhdw <winstxnhdw@gmail.com>
2023-11-22 21:25:35 +0800

    chore: fix import sorting

--- server/features/extraction/__init__.py
+++ server/features/extraction/__init__.py
@@ -1,7 +1,6 @@
-from server.features.extraction.extract_from_pdf import (
-    extract_texts_from_requests as extract_texts_from_requests,
-)
-
 from server.features.extraction.extract_from_image import (
-    extract_texts_from_image_requests as extract_texts_from_image_requests
+    extract_texts_from_image_requests as extract_texts_from_image_requests,
+)
+from server.features.extraction.extract_from_pdf import (
+    extract_texts_from_pdf_requests as extract_texts_from_pdf_requests,
 )
