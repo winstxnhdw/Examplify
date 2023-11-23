f17e1213bad15e133232f2fe64f06dfe7c8869f7
winstxnhdw <winstxnhdw@gmail.com>
2023-11-24 06:00:08 +0800

    fix: update `/generate` to use correct prompt template

--- server/api/v1/generate.py
+++ server/api/v1/generate.py
@@ -13,7 +13,10 @@ def generate(request: Generate) -> StreamingResponse:
     the `/generate` route provides an endpoint for generating text directly from the LLM model
     """
     prompts = (
-        f'<s>[INST] {instruction} [\\INST]'
+        LLM.tokeniser.apply_chat_template([{
+            'role': 'user',
+            'content': instruction
+        }], tokenize=False, add_generation_prompt=True)
         for instruction in request.instructions
     )
 
