7e4a21a3c01920c432b5ddad28fbd2de53cea861
winstxnhdw <winstxnhdw@gmail.com>
2023-11-27 15:10:06 +0800

    feat: add test for `question_answering`

--- server/features/query/query.py
+++ server/features/query/query.py
@@ -1,31 +1,8 @@
-from typing import Awaitable, Callable, Iterable
+from typing import Awaitable, Callable
 
 from server.features.llm import LLM
 from server.features.llm.types import Message
-
-
-def question_answering(
-    messages: list[Message],
-    chain: Callable[[Iterable[Message]], Message | None]
-) -> list[Message]:
-    """
-    Summary
-    -------
-    ask a question and get an answer
-
-    Parameters
-    ----------
-    messages (list[Message]): the message history
-    chain (Callable[[Iterable[Message]], Message | None]): the model
-
-    Returns
-    -------
-    messages (list[Message]): the message history
-    """
-    while not (answer := chain(messages)):
-        messages = messages[1:]
-
-    return messages + [answer]
+from server.features.query.question_answering import question_answering
 
 
 async def query_llm(
---
+++ tests/test_question_answering.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-function-docstring
+
+from server.features.llm.types import Message
+from server.features.query.query import question_answering
+
+
+def chain(messages: list[Message]) -> Message | None:
+
+    if len(messages) > 1:
+        return None
+
+    return {'role': 'assistant', 'content': 'Hello world!'}
+
+
+def test_question_answering():
+
+    messages: list[Message] = [
+        {'role': 'assistant', 'content': 'Hello world!'},
+        {'role': 'user', 'content': 'Hello world!'}
+    ]
+
+    answers = question_answering(messages, chain)
+
+    assert len(answers) == 2
+    assert answers[-1]['role'] == 'assistant'
