b6a515b54bd2132482b056ca153ace737faf702a
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-03-07 04:35:40 +0800

    chore: app is not meant to be run with workers

--- server/config.py
+++ server/config.py
@@ -40,7 +40,6 @@ class Config(BaseSettings):
     """
     server_port: int = 49494
     server_root_path: str = '/api'
-    worker_count: int = 1
     redis_index_name: str = 'index'
     use_cuda: bool = False
     document_index_prefix: str = 'doc'
