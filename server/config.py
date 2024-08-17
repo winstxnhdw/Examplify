711468f6871c14c87d025298d423b573df430175
winstxnhdw <winstxnhdw@gmail.com>
2024-08-18 02:27:25 +0800

    refactor(config): remove redundant `cache` decorator

--- server/config.py
+++ server/config.py
@@ -1,27 +1,6 @@
-from functools import lru_cache
-from typing import Callable
-
 from pydantic_settings import BaseSettings
 
 
-def cache[T](user_function: Callable[[type[T]], T]) -> Callable[[type[T]], T]:
-    """
-    Summary
-    -------
-    a specific decorator to cache the result of a function
-
-    Parameters
-    ----------
-    user_function (Callable[[type[T]], T]) : the function to cache
-
-    Returns
-    -------
-    wrapper (Callable[[type[T]], T]) : the wrapper function
-    """
-    return lru_cache(maxsize=None)(user_function)
-
-
-@cache
 def singleton[T](cls: type[T]) -> T:
     """
     Summary
@@ -59,9 +38,9 @@ class Config(BaseSettings):
     """
 
     server_port: int = 49494
-    server_root_path: str = '/api'
-    redis_index_name: str = 'index'
+    server_root_path: str = "/api"
+    redis_index_name: str = "index"
     use_cuda: bool = False
-    document_index_prefix: str = 'doc'
-    document_index_tag: str = 'chat_id'
+    document_index_prefix: str = "doc"
+    document_index_tag: str = "chat_id"
     embedding_dimensions: int = 768
